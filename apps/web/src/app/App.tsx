53208a0091a4143df6bb6dc4093f477984cdc49b
dias002 <dias.sun666@gmail.com>
2026-07-14 16:26:07 +0500

    Fix production API proxy and update prompt

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -5,7 +5,6 @@ import { LanguageProvider } from "./i18n";
 import { AuthProvider } from "./auth";
 import { ThemeProvider } from "./theme";
 import CookieConsentBanner from "./components/CookieConsentBanner";
-import UpdateAvailableToast from "./components/UpdateAvailableToast";
 import CountryConfirmDialog from "./components/CountryConfirmDialog";
 import YandexAnalytics from "./components/YandexAnalytics";
 
@@ -19,7 +18,6 @@ export default function App() {
           </Suspense>
           <CountryConfirmDialog />
           <CookieConsentBanner />
-          <UpdateAvailableToast />
           <YandexAnalytics />
         </AuthProvider>
       </ThemeProvider>
